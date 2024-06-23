7dd647fb8bcbac0b71b7a7d608bb26af6f3b3cb1
BitByLogic <33465177+BitByLogics@users.noreply.github.com>
2024-06-23 16:13:19 -0400

    Total overhaul!

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# SilktouchSpawners
-SilktouchSpawners minecraft plugin
+<div align="center">
+<img src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/c/cf/Spawner_with_fire.png">
+<h1>Silktouch Spawners</h1>
+</div>
+
+## What is Silktouch Spawners?
+Silktouch Spawners is a Minecraft plugin designed to allow players to mine and pick up
+mob spawners using the Silk Touch enchantment. This plugin adds a new level of depth and
+strategy to mob farming and spawner management.
+
+## Features
+
+- Mine and collect mob spawners using the Silk Touch enchantment
+- Customizable lore and display name for mined spawners
+- Ability to allow one-time use items to pick up spawners
+- Integration with other plugins for protection and custom behavior
+
+## Gifs
+
+<div style="display: flex; justify-content: space-between;">
+    <div style="text-align: center; margin-right: 10px;">
+        <p>Ability to use Silktouch to grab spawners</p>
+        <img src="https://imgur.com/Ud8aFwK.gif" alt="Image 1" style="width: 100%;">
+    </div>
+    <div style="text-align: center;">
+        <p>One time use custom items</p>
+        <img src="https://i.imgur.com/BgfaEQi.gif" alt="Image 2" style="width: 100%;">
+    </div>
+</div>
+
+## Commands
+
+| Command                                                         | Description                                                      |
+|-----------------------------------------------------------------|------------------------------------------------------------------|
+| /silktouchspawners give <player> <spawner type> [amount]        | Gives the specified player the specified amount of spawners.     |
+| /silktouchspawners givecustomitem <player> <item name> [amount] | Gives the specified player the specified amount of custom items. |
+| /silktouchspawners reloadconfig                                 | Reloads the plugin configuration file.                           |
+
+## Permissions
+
+| Permission                      | Description                                                                         |
+|---------------------------------|-------------------------------------------------------------------------------------|
+| silktouchspawners.mine.<entity> | Allows the player to mine and pick up spawners with Silk Touch.                     |
+| silktouchspawners.mine.all      | Allows the player to mine all types of spawners.                                    |
+| silktouchspawners.give          | Allows the player to use the give commands to distribute spawners and custom items. |
+
+# Like it? Download from [Spigot](https://www.spigotmc.org/resources/silktouchspawners-1-20.117535/)!
--- src/main/java/me/niko302/silktouchspawners/commands/CustomItemCommand.java
+++ src/main/java/me/niko302/silktouchspawners/commands/CustomItemCommand.java
@@ -1,48 +1,59 @@
 package me.niko302.silktouchspawners.commands;
 
-import me.niko302.silktouchspawners.silktouchspawners;
+import lombok.RequiredArgsConstructor;
 import me.niko302.silktouchspawners.config.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.StringUtil;
 
-public class CustomItemCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    private final silktouchspawners plugin;
+@RequiredArgsConstructor
+public class CustomItemCommand implements TabExecutor {
 
-    public CustomItemCommand(silktouchspawners plugin) {
-        this.plugin = plugin;
-    }
+    private final ConfigManager configManager;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 2) {
             sender.sendMessage("Usage: /givecustomitem <player> <itemname>");
-            return false;
+            return true;
         }
 
         Player targetPlayer = Bukkit.getPlayer(args[0]);
+
         if (targetPlayer == null) {
             sender.sendMessage("Player not found.");
-            return false;
+            return true;
         }
 
-        ItemStack customItem = plugin.getConfigManager().getCustomItem(args[1]);
-        if (customItem == null) {
-            sender.sendMessage("Custom item not found.");
-            return false;
-        }
+        configManager.getCustomItem(args[1]).ifPresentOrElse(customItem -> {
+            if (targetPlayer.getInventory().addItem(customItem).isEmpty()) {
+                sender.sendMessage("Gave custom item to " + targetPlayer.getName() + ".");
+                return;
+            }
 
-        if (targetPlayer.getInventory().addItem(customItem).isEmpty()) {
-            sender.sendMessage("Gave custom item to " + targetPlayer.getName() + ".");
-        } else {
             targetPlayer.getWorld().dropItemNaturally(targetPlayer.getLocation(), customItem);
-            targetPlayer.sendMessage(ConfigManager.translateColorCodes(plugin.getConfigManager().getSpawnerDropMessage()));
+            targetPlayer.sendMessage(configManager.getSpawnerDropMessage());
+        }, () -> sender.sendMessage("Custom item not found."));
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
+        if (args.length == 1) {
+            return StringUtil.copyPartialMatches(args[0], Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toSet()), new ArrayList<>());
         }
 
-        return true;
+        if (args.length == 2) {
+            return StringUtil.copyPartialMatches(args[1], configManager.getCustomItems().keySet(), new ArrayList<>());
+        }
+
+        return new ArrayList<>();
     }
 }
